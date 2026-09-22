ee4a24717c66f093a773e798ca5445c885fa4d7d
DanielPM <danpame1219@gmail.com>
2026-09-22 09:38:56 -0500

    add(chore): Initial project structure

---
+++ src/main/java/edu/co/escuelaing/containerizing_and_deploying_a_java_web_app/ContainerizingAndDeployingAJavaWebAppApplication.java
@@ -0,0 +1,13 @@
+package edu.co.escuelaing.containerizing_and_deploying_a_java_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizingAndDeployingAJavaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerizingAndDeployingAJavaWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/co/escuelaing/containerizing_and_deploying_a_java_web_app/ContainerizingAndDeployingAJavaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.co.escuelaing.containerizing_and_deploying_a_java_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerizingAndDeployingAJavaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
